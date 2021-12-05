9ed66e327bbb3cbdc87426f1210e3543b84f4a8a
Merlin Scholz <merlin@scholz.ruhr>
2021-12-05 20:43:09 +0100

    Improve 03b

--- 03b/main.py
+++ 03b/main.py
@@ -1,24 +1,16 @@
 if __name__ == '__main__':
     input = [[int(y) for y in x.strip()] for x in open('input', 'r').readlines()]
 
-    # Oxygen
     oxygen = input.copy()
-    for i in range(len(oxygen[0])):
-        if sum([x[i] for x in oxygen]) >= len(oxygen) / 2:
-            oxygen = [x for x in oxygen if x[i] == 1]
-        else:
-            oxygen = [x for x in oxygen if x[i] == 0]
-
-        if(len(oxygen) == 1):
-            oxygen_final = int(''.join(str(x) for x in oxygen[0]), 2)
     co2 = input.copy()
-    for i in range(len(co2[0])):
-        if sum([x[i] for x in co2]) < len(co2) / 2:
-            co2 = [x for x in co2 if x[i] == 1]
-        else:
-            co2 = [x for x in co2 if x[i] == 0]
 
-        if(len(co2) == 1):
-            c02_final = int(''.join(str(x) for x in co2[0]), 2)
+    for i in range(len(oxygen[0])):
+        if(len(oxygen) > 1):
+            oxygen = [x for x in oxygen if x[i] == int(sum([x[i] for x in oxygen]) >= len(oxygen) / 2)]
+        if(len(co2) > 1):
+            co2 =    [x for x in co2    if x[i] == int(sum([x[i] for x in co2   ]) <  len(co2)    / 2)]
 
-    print(oxygen_final * c02_final)
+        if(len(oxygen) == 1 and len(co2) == 1):
+            print(int(''.join(str(x) for x in oxygen[0]), 2) *
+                  int(''.join(str(x) for x in co2[0]   ), 2))
+            break
