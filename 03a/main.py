3fa08a94fb0fd49737ffb78070614c0a03956194
Merlin Scholz <merlin@scholz.ruhr>
2021-12-07 23:41:41 +0100

    Remove string methods from day 3a

--- 03a/main.py
+++ 03a/main.py
@@ -3,8 +3,9 @@ import numpy as np
 if __name__ == '__main__':
     input = np.array(np.genfromtxt('input', delimiter=1, dtype=np.uint8), dtype=bool)
     bitsum = np.sum(input, axis=0)
+    r = len(input)/2
 
-    gamma = np.vectorize(lambda x: 1 if x > (len(input)/2) else 0)(bitsum)
+    gamma = np.vectorize(lambda x: int(x > r))(bitsum)
     gamma = gamma.dot(1 << np.arange(gamma.shape[-1] - 1, -1, -1))
     epsilon = 2**len(input[0])-1 ^ gamma
 
