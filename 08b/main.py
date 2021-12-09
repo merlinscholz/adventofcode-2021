d00cd86c781bc49754b637838e7a31193c8278ad
Merlin Scholz <merlin@scholz.ruhr>
2021-12-09 01:00:35 +0100

    Day 8b first cleanup

--- 08b/main.py
+++ 08b/main.py
@@ -3,60 +3,47 @@ from typing import List
 import numpy as np
 
 if __name__ == '__main__':
-    input1all = [[set(y) for y in x.split('|')[0].split()] for x in open('input', 'r').readlines()]
-    input2all = [[set(y) for y in x.split('|')[1].split()] for x in open('input', 'r').readlines()]
-    lengthsall = np.vectorize(len)(input1all)
+    input = np.array([[set(y) for y in x.split()[:]] for x in open('input', 'r').readlines()])
 
-    # TODO Iterate over input lines
 
-    output = [0] * len(input1all)
-    for line in range(len(input1all)):
-        input1 = np.array(input1all[line])
-        input2 = np.array(input2all[line])
-        lengths = np.array(lengthsall[line])
+    def calc(input):
+        lengths = list(np.vectorize(len)(input))
 
-        mapping = {}
-        a: list[set] = [set] * 10
-
-        a[1] = set(input1[lengths == 2][0])
-        a[4] = set(input1[lengths == 4][0])
-        a[7] = set(input1[lengths == 3][0])
-        a[8] = set(input1[lengths == 7][0])
-
-        mapping[(a[7] - a[1]).pop()] = 'a'
+        a: list[set] = [None] * 11 # a[10] is the minus sign, needed to convert 8 to 0
+        a[1] = set(input[lengths.index(2)])
+        a[4] = set(input[lengths.index(4)])
+        a[7] = set(input[lengths.index(3)])
+        a[8] = set(input[lengths.index(7)])
 
         # Nine
         nine_missing_g = a[4] | a[7]
         for x in a[8] - nine_missing_g:
-            if set(x) | nine_missing_g in input1:
+            if set(x) | nine_missing_g in input:
                 a[9] = set(x) | nine_missing_g
-                mapping[x] = 'g'
 
         # Three
         three_missing_d = a[7] | (a[9] - (a[4] | a[7]))
         for x in a[8] - three_missing_d:
-            if set(x) | three_missing_d in input1:
+            if set(x) | three_missing_d in input:
                 a[3] = set(x) | three_missing_d
-                mapping[x] = 'd'
+                a[10] = set(x)
 
         # Six
-        for x in input1:
+        for x in input:
             if x | a[1] == a[8] and x != a[8]:
                 a[6] = x
-                mapping[(a[1] - a[6]).pop()] = 'c'
-
-        mapping[(a[1] & a[6]).pop()] = 'f'
-
-        reverse_mapping = {value : key for (key, value) in mapping.items()}
 
         # Five
-        a[5] = a[9] - set(reverse_mapping['c'])
-        a[0] = a[8] - set(reverse_mapping['d'])
-        a[2] = a[3] - set(reverse_mapping['f']) | (a[6] - a[5])
+        a[5] = a[9] - (a[1] - a[6])
+
+        # Zero
+        a[0] = a[8] - a[10]
+
+        # Two
+        a[2] = a[3] - (a[1] & a[6]) | (a[6] - a[5])
+
+        return np.sum([a.index(x)*y for x, y in zip(input[-4:], [10**exp for exp in np.arange(3, -1, -1)])])
 
 
-
-        output[line] = int(''.join(str(a.index(x)) for x in input2))
-
+    output = [calc(x) for x in input]
     print(np.sum(output))
-    pass
