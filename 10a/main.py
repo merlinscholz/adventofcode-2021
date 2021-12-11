0c6a7770a001d3620c3157c7eaae94068faa8133
Merlin Scholz <merlin@scholz.ruhr>
2021-12-11 22:01:20 +0100

    Clean up day 10

--- 10a/main.py
+++ 10a/main.py
@@ -1,28 +1,30 @@
 if __name__ == '__main__':
     input = open('input', 'r').readlines()
 
-    def inverse(char):
-        if char == '(': return ')'
-        if char == '[': return ']'
-        if char == '{': return '}'
-        if char == '<': return '>'
+    inverse = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>'
+    }
 
-    def points(char):
-        if char == ')': return 3
-        if char == ']': return 57
-        if char == '}': return 1197
-        if char == '>': return 25137
+    points = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+    }
 
     errors = list()
 
     for line in input:
         stack = list()
         for char in line:
-            if char in ['(', '[', '{', '<']:
+            if char in inverse.keys():
                 stack.append(char)
-            if char in [')', ']', '}', '>']:
-                if inverse(stack.pop(-1)) != char:
+            if char in points.keys():
+                if inverse[stack.pop(-1)] != char:
                     errors.append(char)
                     break
 
-    print(sum([points(x) for x in errors]))
+    print(sum([points[x] for x in errors]))
--- 10b/main.py
+++ 10b/main.py
@@ -1,33 +1,34 @@
 if __name__ == '__main__':
     input = open('input', 'r').readlines()
 
-    def inverse(char):
-        if char == '(': return ')'
-        if char == '[': return ']'
-        if char == '{': return '}'
-        if char == '<': return '>'
+    inverse = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>'
+    }
 
-    def points(char):
-        if char == ')': return 1
-        if char == ']': return 2
-        if char == '}': return 3
-        if char == '>': return 4
+    points = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
 
-    score = lambda x: points(x[0]) if len(x) == 1 else 5*score(x[1:]) + points(x[0])
+    score = lambda x: points[x[0]] if len(x) == 1 else 5*score(x[1:]) + points[x[0]]
 
     scores = list()
 
     for line in input:
         stack = list()
         for char in line:
-            if char in ['(', '[', '{', '<']:
+            if char in inverse.keys():
                 stack.append(char)
-            if char in [')', ']', '}', '>']:
-                if inverse(stack.pop(-1)) != char:
+            if char in points.keys():
+                if inverse[stack.pop(-1)] != char:
                     stack = list()
                     break
 
-        if len(stack) > 0: scores.append(score([inverse(x) for x in stack]))
+        if len(stack) > 0: scores.append(score([inverse[x] for x in stack]))
 
     print(sorted(scores)[len(scores)//2])
-    pass
