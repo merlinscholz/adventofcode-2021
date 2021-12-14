d6fa4e8bbe938cddd3558038e5f80d7a4852c5f3
Merlin Scholz <merlin@scholz.ruhr>
2021-12-14 21:44:43 +0100

    Start day 14 alg rework

--- 14a/main.py
+++ 14a/main.py
@@ -1,17 +1,25 @@
 from collections import Counter
 
 if __name__ == '__main__':
-    input = [x.strip() for x in open('input').readlines()]
-    working = [x for x in input[0]]
+    input = [x.strip() for x in open('test').readlines()]
+    tuples = {}
+    for i in range(len(input[0])-1):
+        values = "".join(input[0][i:i+2])
+        if values not in tuples.keys(): tuples[values] = 0
+        tuples[values] += 1
+
+    # TODO Map to tuple of split values
+
     rules = {x.split(' -> ')[0]: x.split(' -> ')[1] for x in input[2:]}
 
-    for iteration in range(10):
-        append = list()
-        delta = 0
-        for i in range(len(working)-1):
-            if "".join(working[i+delta:i+delta+2]) in rules.keys():
-                working.insert(i + delta+1, rules["".join(working[i+delta:i+delta+2])])
-                delta += 1
+    for iteration in range(1):
+        iter = tuples.copy()
+        for tuple in iter.keys():
+            if tuple in rules.keys():
+                tuples[tuple] -= iter[tuple]
+                if tuple[0] + rules[tuple] not in tuples.keys(): tuples[tuple[0] + rules[tuple]] = 0
+                tuples[tuple[0] + rules[tuple]] += iter[tuple]
+                if rules[tuple] + tuple[1] not in tuples.keys(): tuples[rules[tuple] + tuple[1]] = 0
+                tuples[rules[tuple] + tuple[1]] += iter[tuple]
 
-    counter = Counter(working)
-    print(counter.most_common()[0][1] - counter.most_common()[-1][1])
+    print(tuples)
